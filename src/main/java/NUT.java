12300e5bc2fce3930ed35f91af9200fad1a9fda6
hazelhidayah <hidayah03@hotmail.sg>
2026-01-22 01:49:52 +0800

    completed level-1

--- src/main/java/NUT.java
+++ src/main/java/NUT.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class NUT {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n" +
                 " Hello! I'm NUT\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println(logo);
+
+        Scanner sc = new Scanner(System.in); // read user input
+
+        while (true) { // continue till false
+            String userInput = sc.nextLine();
+
+            // the user typed 'bye'
+            // end conversation when user says 'bye'.
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("okey byebye");
+                break; // to terminate
+            }
+            System.out.println(userInput);
+        }
+
+        sc.close();
     }
 }
