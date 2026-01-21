fa8346e90cd0e06b8639755791c055b364f0a04c
hazelhidayah <hidayah03@hotmail.sg>
2026-01-22 03:47:57 +0800

    completed task2git statusgit status

--- src/main/java/NUT.java
+++ src/main/java/NUT.java
@@ -1,25 +1,63 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NUT {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n" +
-                " Hello! I'm NUT\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n";
+        String logo = """
+                ____________________________________________________________
+                 Hello! I'm NUT
+                 What can I do for you?
+                ____________________________________________________________
+                """;
         System.out.println(logo);
 
         Scanner sc = new Scanner(System.in); // read user input
 
-        while (true) { // continue till false
+        //String[] list = new String[100];
+        ArrayList<String> tasks = new ArrayList<>();
+
+        while (tasks.size() < 100) { // continue till too many tasks
             String userInput = sc.nextLine();
 
             // the user typed 'bye'
-            // end conversation when user says 'bye'.
+            // end conversation
             if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("okey byebye");
+                System.out.println("    ____________________________________________________________\n");
+                System.out.println("    okay bai");
+                System.out.println("    ____________________________________________________________\n");
                 break; // to terminate
             }
-            System.out.println(userInput);
+
+            // if input is 'list'
+            // print the list
+            else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("    ____________________________________________________________\n");
+
+                if (tasks.isEmpty()) { // no tasks
+                    System.out.println("    You currently have 0 things to do, yay");
+                } else { // not empty
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("    " + (i+1) + ". " + tasks.get(i));
+                    }
+                }
+                System.out.println("    ____________________________________________________________\n");
+                // System.out.println(userInput);
+            }
+
+            else { // they gave tasks
+                tasks.add(userInput);
+                System.out.println("    ____________________________________________________________\n");
+                System.out.println("    added: " + userInput + "\n");
+                System.out.println("    ____________________________________________________________\n");
+            }
+        }
+
+        // break cause too many tasks or user said bye
+
+        if (tasks.size() > 99) { // max out
+            System.out.println("    ____________________________________________________________\n");
+            System.out.println("    You're too overwhelmed! No more tasks for you >:( \n");
+            System.out.println("    ____________________________________________________________\n");
         }
 
         sc.close();
